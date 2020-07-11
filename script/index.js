9d3f2abab71e32a64cb6f7d03fd0f2407a815591
Mansur <mansur98@yandex.ru>
2020-07-11 21:04:52 +0400

    Работа над ошибками_1

--- script/index.js
+++ script/index.js
@@ -1,24 +1,32 @@
 const openModalButton = document.querySelector('.profile__edit-button');
 const modal = document.querySelector('.modal');
 const closeModalButton = document.querySelector('.modal__close-button');
-const modalName = document.querySelector('.modal__name');
-const modalProfession = document.querySelector('.modal__profession');
-const modalsubmit = document.querySelector('.modal__submit');
+const modalName = document.querySelector('.modal__input_object_name');
+const modalProfession = document.querySelector('.modal__input_object_profession');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
+const form = document.querySelector('.modal__container');
 
-openModalButton.addEventListener('click', () => {
+function ModalOpen(event) {
   modal.classList.add('modal_is-open');
   modalName.value = profileName.textContent;
   modalProfession.value = profileProfession.textContent;
-})
+}
 
-closeModalButton.addEventListener('click', () => {
-  modal.classList.remove('modal_is-open'); 
-})
+function ModalClose(event) {
+  modal.classList.remove('modal_is-open');
+  event.preventDefault();
+}
 
-modalsubmit.addEventListener('click', () => {
+function ModalSave(event) {
   profileName.textContent = modalName.value;
   profileProfession.textContent = modalProfession.value;
-  modal.classList.remove('modal_is-open');
-})
+  ModalClose(event);
+  event.preventDefault();
+}
+
+openModalButton.addEventListener('click', ModalOpen)
+
+closeModalButton.addEventListener('click', ModalClose)
+
+form.addEventListener('submit', ModalSave)
